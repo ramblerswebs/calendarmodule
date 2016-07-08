c19e3c495b4babffc4139139e0aa249890e76668
Keith Grimes <Keith.Grimes@kcgrimes.co.uk>
2016-07-08 21:36:25 +0100

    Correct termination of header

--- calendar_download.php
+++ calendar_download.php
@@ -4,7 +4,7 @@
  * File       calendar_download.php
  * Created    1/17/14 12:29 PM
  * Author     Keith Grimes | webmaster@wiltsswindonramblers.org.uk | http://wiltsswindonramblers.org.uk
- * Support    
+ * Support
  * License    GNU General Public License version 2, or later.
  */
 
@@ -13,8 +13,8 @@ $output = $_POST["icsdata"];
 
 $length = strlen($output);
 
-header("Content-type:text/calendar");
-header('Content-Disposition: attachment; filename=walks.ics"');
+header('Content-type:text/calendar');
+header('Content-Disposition: attachment; filename=walks.ics');
 header('Content-Length: '.$length);
 echo $output;
 ?>
